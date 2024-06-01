7dce1d76e281fb10245ebb92cd702b36957991e5
ZakhaevK <zkupke@gmail.com>
2024-06-01 15:49:51 +1000

    Create function getComputerChoice Also includes pseudocode update for getPlayerChoice

--- main.js
+++ main.js
@@ -1,1 +1,14 @@
-console.log("Garn")
+// Generates a random computer choice of rock, paper, or scissors.
+function getComputerChoice() {
+  const num = Math.random();
+  let choice;
+
+  if (num <= 0.33) {
+    choice = "rock";
+  } else if (num <= 0.66) {
+    choice = "paper";
+  } else {
+    choice = "scissors";
+  }
+  return choice;
+}
